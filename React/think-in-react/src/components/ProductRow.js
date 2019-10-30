679c0d5836e86f692c05e27fff6c6d14e6fb3941
woori <skdlwn156@naver.com>
2019-10-30 13:59:04 +0900

    unsplash complete

--- React/think-in-react/src/components/ProductRow.js
+++ React/think-in-react/src/components/ProductRow.js
@@ -3,9 +3,12 @@ import React from "react";
 const ProductRow = (name, keyword, stocked, price, checked) => {
   const lowerdName = name.toLowerCase();
   const lowerdKeyword = keyword.toLowerCase();
+  //  const reg = new RegExp(keyword, "i");
+  //  const reg2 = /test/i;
   if (checked && !stocked) return null;
   if (!lowerdName.includes(lowerdKeyword)) return null;
   //  if (!new RegExp(`/${props.name}/i`).test(props.keyword))
+  //  if (!reg.test(name)) return null;
   return (
     <tr>
       <td style={{ color: !stocked && "red" }}>{name}</td>
--- React/unsplash/src/components/App.js
+++ React/unsplash/src/components/App.js
@@ -6,7 +6,14 @@ import unsplash from "../api/unsplash";
 export default class App extends Component {
   state = {
     keyword: "",
-    images: []
+    images: [],
+    page: 1,
+    number: 0
+  };
+
+  handlePage = e => {
+    if (e) this.setState({ page: this.state.page + 1 });
+    else this.setState({ page: this.state.page - 1 });
   };
 
   handleKeyword = word => {
@@ -15,7 +22,7 @@ export default class App extends Component {
 
   onSubmit = async () => {
     const response = await unsplash.get("search/photos", {
-      params: { query: this.state.keyword }
+      params: { query: this.state.keyword, page: this.state.page, per_page: 5 }
     });
     this.setState({ images: response.data.results });
   };
@@ -23,11 +30,48 @@ export default class App extends Component {
   render() {
     return (
       <div className="ui container">
+        <button
+          className="ui button"
+          onClick={async () => {
+            console.log(this.state.number);
+            await this.setState({ number: this.state.number + 1 });
+            console.log(this.state.number);
+          }}
+        >
+          스테이트변경!
+        </button>
         <SearchBar
           handleKeyword={this.handleKeyword}
           onSubmit={this.onSubmit}
         />
-        <ImageList images={this.state.images} />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {/* 삼항연산자 condition ? 참일때 : 거짓일때
+          if(condition) 참일때;
+          else 거짓일때 */}
+          {this.state.page === 1 ? null : (
+            <button
+              className="ui button"
+              onClick={async () => {
+                await this.handlePage(false);
+                this.onSubmit();
+              }}
+            >
+              이전페이지
+            </button>
+          )}
+          <ImageList images={this.state.images} />
+          {this.state.images.length === 0 ? null : (
+            <button
+              className="ui button"
+              onClick={async () => {
+                await this.handlePage(true);
+                this.onSubmit();
+              }}
+            >
+              다음페이지
+            </button>
+          )}
+        </div>
       </div>
     );
   }
