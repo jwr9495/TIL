df74aa30a4de32aaae380fe61c3e4944161596df
jwr9495 <skdlwn156@naver.com>
2019-11-05 15:05:33 +0900

    mongodb connection complete

---
+++ MongoDB/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB connection complete ...."))
+  .catch(error => console.error(error));
